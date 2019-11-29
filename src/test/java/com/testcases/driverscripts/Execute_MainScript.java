ba1bbb016769ae6e6ff410da3ce1aa0ba7377af8
RKLogixal <rohit.karkhanis@logixal.com>
2019-11-29 15:15:17 +0530

    updates

--- src/test/java/com/testcases/driverscripts/Execute_MainScript.java
+++ src/test/java/com/testcases/driverscripts/Execute_MainScript.java
@@ -185,14 +185,15 @@ public class Execute_MainScript {
 
 				if(uc.OS.equalsIgnoreCase("Linux")) {
 
-					System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +"/Browser_files/chromedriver_linux64/chromedriver");
-					//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +"/Browser_files/chromedriver_win32/chromedriver.exe");
+					//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +"/Browser_files/chromedriver_linux64/chromedriver");
+					System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +"/Browser_files/chromedriver_win32/chromedriver.exe");
 					ChromeOptions options = new ChromeOptions();
-					//options.addArguments("--headless");
+					options.addArguments("--headless");
 					options.addArguments("--no-sandbox");
 					options.addArguments("--disable-dev-shm-usage");
+					options.addArguments("window-size=1024,768");
 					webdriver = new ChromeDriver(options);
-					webdriver.manage().window().maximize();
+					//webdriver.manage().window().maximize();
 				}
 
 				else if (uc.OS.equalsIgnoreCase("Windows")) {
