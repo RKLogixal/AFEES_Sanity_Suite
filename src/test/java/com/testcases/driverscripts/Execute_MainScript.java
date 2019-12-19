7135f3729b6d10cad202537f3d6a9368a16af914
RKLogixal <rohit.karkhanis@logixal.com>
2019-12-19 18:41:25 +0530

    Update

--- src/test/java/com/testcases/driverscripts/Execute_MainScript.java
+++ src/test/java/com/testcases/driverscripts/Execute_MainScript.java
@@ -192,7 +192,7 @@ public class Execute_MainScript {
 					/*options.addArguments("--no-sandbox");
 					options.addArguments("--disable-dev-shm-usage");
 					options.addArguments("window-size=1364,768");*/
-					options.addArguments("start-maximized"); // open Browser in maximized mode
+					//options.addArguments("start-maximized"); // open Browser in maximized mode
 					options.addArguments("disable-infobars"); // disabling infobars
 					options.addArguments("--disable-extensions"); // disabling extensions
 					options.addArguments("--disable-gpu"); // applicable to windows os only
